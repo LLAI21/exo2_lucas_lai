8a25e1c21f592b8200235eb9c9cc1a4abff56870
LLAI21 <lucas.travail21@gmail.com>
2025-10-17 16:27:09 +0200

    new update in verifSQL.js

--- src/data/verifSQL.js
+++ src/data/verifSQL.js
@@ -4,12 +4,9 @@ export async function MySQL() {
     try {
         await db.initDB();
 
-        const taskId = await db.addTask("Apprendre Node.js avec MySQL");
-
         const tasks = await db.getTasks();
         console.log("Toutes les tâches :", tasks);
 
-        await db.deleteTask(taskId);
 
         const tasksAfterDelete = await db.getTasks();
         console.log("Tâches après suppression :", tasksAfterDelete);
