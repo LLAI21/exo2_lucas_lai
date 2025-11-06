5f9739081493242c623a53ec7ad917e65c24b01c
LLAI21 <lucas.travail21@gmail.com>
2025-11-06 18:18:43 +0100

    update server.js with swagger

--- server.js
+++ server.js
@@ -4,13 +4,24 @@ import dotenv from 'dotenv';
 import router from './src/routes/mesRoutes.js';
 import connectDB from './src/config/db.js'
 import connectSQL from './src/data/verifSQL.js'
+import swaggerUi from 'swagger-ui-express';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const swaggerDocument = require('./swagger.json');
 
 
+//Appeler dotenv
 dotenv.config();
-const app = express();
-app.use(cors());
-app.use(express.json());
 
+// Initialisation de l'application Express
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use('/api/todos', router); // ou juste app.use(router) selon ta structure
+
+
+// Documentation Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //Connexion à MongoDB
 connectDB();
